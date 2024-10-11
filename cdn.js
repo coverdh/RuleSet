3bb5e751b4dac765b2b86523c6e9e2383b646f51
coverdh <cover_dh@qq.com>
2024-10-11 15:48:49 +0800

    Create cdn.js

---
+++ cdn.js
@@ -0,0 +1,8 @@
+let headers = $request.headers;
+if (headers['referer'] == 'https://pre-emas.console.aliyun.com/'){
+	let new_headers = Object.assign({},headers);
+  headers['Host'] = '59.82.29.3';
+	$done({new_headers});
+}else {
+	$done({headers});
+}
