e2d9cc22ee5fda2be495d8bad52e7c3acd4fbfd9
coverdh <cover_dh@qq.com>
2024-10-11 15:54:49 +0800

    Update cdn.js

--- cdn.js
+++ cdn.js
@@ -1,7 +1,7 @@
 let headers = $request.headers;
 if (headers['referer'] == 'https://pre-emas.console.aliyun.com/'){
 	let new_headers = Object.assign({},headers);
-  headers['Host'] = '59.82.29.3';
+  	new_headers['Host'] = '59.82.29.3';
 	$done({new_headers});
 }else {
 	$done({headers});
