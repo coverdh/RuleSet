f6a460262061c1538263b0711912fe8bd22564fb
coverdh <cover_dh@qq.com>
2024-10-11 16:11:45 +0800

    Update cdn.js

--- cdn.js
+++ cdn.js
@@ -1,5 +1,5 @@
 let headers = $request.headers;
-if (headers['referer'] == 'https://pre-emas.console.aliyun.com/'){
+if (headers['Referer'].startsWith( 'https://pre-emas.console.aliyun.com/')){
 	let urlObj = new URL($request.url);
     	urlObj.hostname = '59.82.29.3';
 	let url = urlObj.toString();
